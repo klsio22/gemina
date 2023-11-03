832e02fbcc2e86d82f8ce6493a3a31e1d7179df1
Klesio dev <klesio.dev@gmail.com>
2023-11-03 18:11:05 -0300

    feat(top-page): created section banner

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -1,9 +1,7 @@
-import Icon from '$store/components/ui/Icon.tsx';
-import Slider from '$store/components/ui/Slider.tsx';
-import SliderJS from '$store/islands/SliderJS.tsx';
 import { useId } from '$store/sdk/useId.ts';
 import type { ImageWidget } from 'apps/admin/widgets.ts';
 import { Picture, Source } from 'apps/website/components/Picture.tsx';
+import { relative } from 'std/path/win32.ts';
 
 /**
  * @titleBy alt
@@ -25,8 +23,23 @@ export interface Banner {
   };
 }
 
+export interface HighlightImages {
+  /** @description desktop otimized image */
+  desktop: ImageWidget;
+  /** @description Image's alt text */
+  alt: string;
+  action?: {
+    /** @description when user clicks on the image, go to this link */
+    href: string;
+    /** @description Image text title */
+  };
+}
 export interface Props {
   images?: Banner[];
+
+  imagesBanners?: HighlightImages[];
+
+  textTitle?: string;
   /**
    * @description Check this option when this banner is the biggest image on the screen for image optimizations
    */
@@ -35,7 +48,6 @@ export interface Props {
    * @title Autoplay interval
    * @description time (in seconds) to start the carousel autoplay
    */
-  interval?: number;
 }
 
 const DEFAULT_PROPS = {
@@ -66,19 +78,6 @@ const DEFAULT_PROPS = {
       desktop:
         'https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/7949d031-9a79-4639-b85e-62fd90af85a9',
     },
-    {
-      alt: '/feminino',
-      action: {
-        href: 'https://www.deco.cx/',
-        label: 'deco.cx',
-        title: 'Demo Store',
-        subTitle: 'Visit our site and start building now:',
-      },
-      mobile:
-        'https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/ae89571c-4a7c-44bf-9aeb-a341fd049d19',
-      desktop:
-        'https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/7ec121e4-5cfe-4b7b-b942-d1ed4493803d',
-    },
   ],
   preload: true,
 };
@@ -90,7 +89,7 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
     <a
       href={action?.href ?? '#'}
       aria-label={action?.label}
-      class='relative h-[600px] overflow-y-hidden w-full'
+      className='relative h-screen overflow-y-hidden w-full'
     >
       <Picture preload={lcp}>
         <Source
@@ -101,15 +100,15 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
           height={600}
         />
         <Source
-          media="(min-width: 768px)"
-          fetchPriority={lcp ? "high" : "auto"}
+          media='(min-width: 768px)'
+          fetchPriority={lcp ? 'high' : 'auto'}
           src={desktop}
           width={1440}
           height={600}
         />
         <img
-          class="object-cover w-full h-full"
-          loading={lcp ? "eager" : "lazy"}
+          className='object-cover w-full h-full'
+          loading={lcp ? 'eager' : 'lazy'}
           src={desktop}
           alt={alt}
         />
@@ -122,44 +121,42 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
     </a>
   );
 }
-/* 
-function Buttons() {
+
+function BannersPhotos({
+  image,
+  lcp,
+}: {
+  image: HighlightImages;
+  lcp?: boolean;
+}) {
+  const { alt, desktop } = image;
+
   return (
-    <>
-      <div class='flex items-center justify-center z-10 col-start-1 row-start-2'>
-        <Slider.PrevButton class='btn btn-circle glass'>
-          <Icon
-            class='text-base-100'
-            size={24}
-            id='ChevronLeft'
-            strokeWidth={3}
-          />
-        </Slider.PrevButton>
-      </div>
-      <div class='flex items-center justify-center z-10 col-start-3 row-start-2'>
-        <Slider.NextButton class='btn btn-circle glass'>
-          <Icon
-            class='text-base-100'
-            size={24}
-            id='ChevronRight'
-            strokeWidth={3}
-          />
-        </Slider.NextButton>
-      </div>
-    </>
-  );
-}
- */
-function BannersPhotos() {
-  return (
-    <div class='flex items-center justify-center z-10 col-start-3 row-start-2'>
-      TESTE
+    <div className='hidden md:flex'>
+      <Picture>
+        <Source
+          media='(min-width: 768px)'
+          src={desktop}
+          fetchPriority={lcp ? 'high' : 'auto'}
+          width={1440}
+          height={600}
+        />
+        <img
+          className='object-cover w-full h-full'
+          loading={lcp ? 'eager' : 'lazy'}
+          src={desktop}
+          alt={alt}
+        />
+      </Picture>
     </div>
   );
 }
 
 function BannerCarousel(props: Props) {
-  const { images, preload, interval } = { ...DEFAULT_PROPS, ...props };
+  const { images, imagesBanners, textTitle, preload } = {
+    ...DEFAULT_PROPS,
+    ...props,
+  };
 
   const id = useId();
 
@@ -168,15 +165,27 @@ function BannerCarousel(props: Props) {
       id={id}
       class='grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px]'
     >
-      <Slider class='carousel carousel-center w-full col-span-full row-span-full gap-6'>
+      <div className='carousel carousel-center w-full col-span-full row-span-full gap-6'>
         {images?.map((image, index) => (
-          <Slider.Item index={index} class='carousel-item w-full'>
-            <BannerItem image={image} lcp={index === 0 && preload} />
-          </Slider.Item>
+          <BannerItem image={image} lcp={index === 0 && preload} />
         ))}
-      </Slider>
-      <BannersPhotos />
-      <SliderJS rootId={id} interval={interval && interval * 1e3} infinite />
+      </div>
+
+      <div className='absolute right-12 top-1/3'>
+        <div>
+          <div className='flex gap-10'>
+            {imagesBanners?.map((image, index) => (
+              <BannersPhotos image={image} lcp={index === 0 && preload} />
+            ))}
+          </div>
+
+          <div className='w-7'>
+            <span className='font-quattrocento font-normal text-xl text-white-matte uppercase'>
+              {textTitle}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
